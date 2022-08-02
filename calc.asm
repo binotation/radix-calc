939e8dc04a039a4f5d97255249e3b0ab18e83b77
binotation <68807919+binotation@users.noreply.github.com>
2022-08-02 20:41:49 +1000

    Added binary output

--- calc.asm
+++ calc.asm
@@ -9,8 +9,10 @@
 %define	in0	bl
 
 section	.bss
-input	db 67 DUP(?)
-halves	dq 65 DUP(?)
+input:	resb 67				; length("0b") + 64 + length("\0")
+i:	resq 1
+halves:	resq 65				; n, n/2, n/4, ..., n/2^64
+rem:	resb 65				; n % 2, n/2 % 2, ..., n/2^64 % 2, "\0"
 
 global	main
 extern	printf
@@ -20,6 +22,7 @@ section	.text
 fmtoct	db "oct: 0%o", 10, 0
 fmtdec	db "dec: %d", 10, 0
 fmthex	db "hex: 0x%x", 10, 0
+fmtbin	db "bin: 0b%s", 10, 0
 
 main:
 	; sys_read(0, input, 67)
@@ -41,6 +44,35 @@ main:
 	xor	arg3, arg3
 	call	strtol
 	mov	[halves], arg0
+	mov	qword [i], 0
+
+construct_binary_loop:			; construct binary representation
+	mov	r8, [i]			; r8 = i
+	mov	r9, [halves + 8 * r8]	; r9 = halves[i]
+	mov	r10, r9
+	shr	r10, 1			; r10 = halves[i] / 2
+	mov	r11, r10
+	shl	r11, 1			; r11 = r10 * 2
+	mov	r12, r9			; r12 = halves[i]
+	sub	r12, r11		; r12 = halves[i] - r10 * 2 = remainder
+	add	r12, 48
+	mov	[rem + r8], r12		; store remainder
+	inc	r8
+	mov	[i], r8			; i++
+	mov	[halves + 8 * r8], r10	; halves[i + 1] = halves[i] / 2
+
+	test	r10, r10
+	jz	print
+	cmp	r8, 65
+	jb	construct_binary_loop
+
+print:
+	mov	byte [rem + r8], 0	; terminate rem string
+	; printf(fmtbin, rem)
+	xor	arg0, arg0
+	mov	arg1, fmtbin
+	mov	arg2, rem
+	call	printf
 
 	; printf(fmtoct, *halves)
 	xor	arg0, arg0
